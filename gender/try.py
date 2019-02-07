8b1f033abf26fb481a644d318127865465a9ff11
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-02-07 17:47:18 +0300

    Updated try.py.

--- gender/try.py
+++ gender/try.py
@@ -3,4 +3,4 @@ import pandas as pd
 
 
 
-gender_data =pd.read_csv("/Users/ilyaperepelitsa/quant/kaggle/gender/data/test_stage_1.tsv", sep = '\t')
+gender_data = pd.read_csv("/Users/ilyaperepelitsa/quant/kaggle/gender/data/test_stage_1.tsv", sep = '\t')

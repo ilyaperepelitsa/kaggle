c6953beedf0f9bbaa076506f47c5b9ff266ccfce
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-02-09 22:39:17 +0300

    Updated try.py.

--- merchant/try.py
+++ merchant/try.py
@@ -18,5 +18,5 @@ transactions.loc[:,transactions.columns.str.contains("_id")].info()
 
 
 
-merchants.loc[:,merchants.columns.str.contains("_id")].info()
-transactions.loc[:,transactions.columns.str.contains("_id")].info()
+merchants.loc[:,~merchants.columns.str.contains("_id")].info()
+transactions.loc[:,~transactions.columns.str.contains("_id")].info()

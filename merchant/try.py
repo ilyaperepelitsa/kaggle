eef91ed0313a70f720df35aea21414797c014d60
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-02-09 21:21:44 +0300

    Updated try.py.

--- merchant/try.py
+++ merchant/try.py
@@ -3,4 +3,4 @@ import pandas as pd
 pd.read_csv("/Volumes/data_pew/numeric/merchant/train.csv")
 # pd.read_csv("/Volumes/data_pew/numeric/merchant/test.csv")
 
-transactions = pd.read_csv("/Volumes/data_pew/numeric/merchant/train.csv")
+transactions = pd.read_csv("/Volumes/data_pew/numeric/merchant/historical_transactions.csv")

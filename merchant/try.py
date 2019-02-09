c16be828786f3dddeb8048c0c75ff8886c180804
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-02-09 22:26:17 +0300

    Updated try.py.

--- merchant/try.py
+++ merchant/try.py
@@ -9,7 +9,7 @@ merchants = pd.read_csv("/Volumes/data_pew/numeric/merchant/merchants.csv")
 
 
 transactions.info()
-transactions.info()
+new_transactions.info()
 
 merchants.info()
 merchants.loc[:,merchants.columns.str.contains("_id")].info()

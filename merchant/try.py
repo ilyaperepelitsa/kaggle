e3151d003075903efa9f866733ba9debeeffa253
Ilya Perepelitsa <ilyaperepelitsa@gmail.com>
2019-02-09 22:25:48 +0300

    Updated try.py.

--- merchant/try.py
+++ merchant/try.py
@@ -4,7 +4,7 @@ pd.read_csv("/Volumes/data_pew/numeric/merchant/train.csv")
 # pd.read_csv("/Volumes/data_pew/numeric/merchant/test.csv")
 
 transactions = pd.read_csv("/Volumes/data_pew/numeric/merchant/historical_transactions.csv")
-merchants = pd.read_csv("/Volumes/data_pew/numeric/merchant/merchants.csv")
+newtransactions = pd.read_csv("/Volumes/data_pew/numeric/merchant/merchants.csv")
 merchants = pd.read_csv("/Volumes/data_pew/numeric/merchant/merchants.csv")
 
 
